d164bf68110f2302d3f387f8139920726bef9d55
sabinabernardes <sabina.ricardok@gmail.com>
2021-01-28 15:17:14 -0300

    organização de cod

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -34,20 +34,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onResume() {
         super.onResume()
 
-        recyclerView = findViewById(R.id.recyclerView)
-        progressBar = findViewById(R.id.user_list_progress_bar)
-
-        adapter = UserListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        progressBar.visibility = View.VISIBLE
+        recyclerViewConfig()
+        progressBarConfig()
 
         val viewModel = ViewModelProvider(this,
             UserViewModel.UserViewModelFactory(UserRepositoryImplementation())) //
             .get(UserViewModel::class.java)
-
-        viewModel.getUsersCoroutines()
 
         viewModel.usersMutableLiveData.observe(this, Observer { users ->
             users?.let {
@@ -59,6 +51,18 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 }
             }
         })
+        viewModel.getUsersCoroutines()
+    }
+    private fun recyclerViewConfig(){
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = UserListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+    }
+    private fun progressBarConfig(){
+        progressBar = findViewById(R.id.user_list_progress_bar)
+        progressBar.visibility = View.VISIBLE
     }
     private fun addUsers(usersAdd: List<User>) {
         progressBar.visibility = View.GONE
@@ -71,4 +75,5 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         Toast.makeText(this@MainActivity, getString(R.string.error), Toast.LENGTH_SHORT)
             .show()
     }
+
 }
